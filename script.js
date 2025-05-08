4176347ea14fdf6246ab0e04c4dd8b2a54e26499
Z4AL <132984247+Z4AL@users.noreply.github.com>
2025-05-08 19:54:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['saya lulusan smk krian 1 tahun 2024 ', 'REKAYASA PERANGKAT LUNAK', 'terima kasih'],
+    strings: ['saya lulusan smk krian 1 tahun 2024 ', 'REKAYASA PERANGKAT LUNAK', ''],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
